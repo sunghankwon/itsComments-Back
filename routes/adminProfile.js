395c48e1fc48ed57ee91248d6387d6ceadcd51c6
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-02-02 19:32:07 +0900

    feat: patch icon

--- routes/adminProfile.js
+++ routes/adminProfile.js
@@ -7,6 +7,16 @@ const verifyToken = require("../middleware/verifyToken");
 router.patch("/user", verifyToken, async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.user.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.icon = req.body.icon || user.icon;
+
+    await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully", user });
   } catch (err) {
     next(err);
   }
