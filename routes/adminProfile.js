8f1bd7b66a7e4743256fbedab32b52a815adeb1b
정든결 <wantchurros@jeongdeungyeol-ui-MacBookAir.local>
2024-02-02 19:23:17 +0900

    feat: verifyToken find user

--- routes/adminProfile.js
+++ routes/adminProfile.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../Models/User");
 
-router.patch("/user", (req, res, next) => {
+const { User } = require("../Models/User");
+const verifyToken = require("../middleware/verifyToken");
+
+router.patch("/user", verifyToken, async (req, res, next) => {
   try {
+    const user = await User.findOne({ email: req.user.email });
   } catch (err) {
     next(err);
   }
