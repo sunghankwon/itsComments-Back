d164a3c7ed4def14caccc8044a3b0626a7670d98
sunghankwon <akvo852@naver.com>
2024-02-06 20:23:35 +0900

    feat: Add response parsing for the API request to the '/comments/new' endpoint

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -2,7 +2,55 @@ const express = require("express");
 
 const router = express.Router();
 
+const { User } = require("../models/User");
 const { Comment } = require("../models/Comment");
+
+router.post("/new", async function (req, res, next) {
+  try {
+    const {
+      userData,
+      description,
+      postDate,
+      postUrl,
+      postCoordinate,
+      imgpath,
+      allowPublic,
+      publicUsers,
+      maillingAddress,
+    } = req.body;
+
+    const user = await User.findOne({ email: userData.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (description.length > 200) {
+      return res
+        .status(400)
+        .json({ message: "The number of characters exceeded 200." });
+    }
+
+    const newComment = await Comment.create({
+      creator: user._id,
+      description,
+      postDate,
+      postUrl,
+      postCoordinate,
+      imgpath,
+      allowPublic,
+      publicUsers,
+      maillingAddress,
+    });
+
+    user.createdPapers.push(newComment._id);
+    await user.save();
+
+    res.status(200).json({ newComment });
+  } catch (error) {
+    return res.status(400).json({ message: "Comment creation failed." });
+  }
+});
 
 router.get("/:commentId", async (req, res) => {
   try {
