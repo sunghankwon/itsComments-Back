cbe321f56087e53c3f889d809f5012abf152f575
JungDeunGyul <ask7384@gmail.com>
2024-02-06 16:15:28 +0900

    feat: get commentId

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,11 +1,13 @@
 const indexRouter = require("../routes/index");
 const loginRouter = require("../routes/login");
 const profileRouter = require("../routes/profile");
+const commentRouter = require("../routes/comment");
 
 async function routerLoader(app) {
   app.use("/", indexRouter);
   app.use("/login", loginRouter);
   app.use("/profile", profileRouter);
+  app.use("/comments", commentRouter);
 }
 
 module.exports = routerLoader;
