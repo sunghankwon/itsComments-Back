d97a969d2a85f06539aed30f5a975cc3a185ac45
sunghankwon <akvo852@naver.com>
2024-02-07 19:51:07 +0900

    fix: Modify User and Comment schemas

--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,18 @@ const userSchema = new mongoose.Schema({
   icon: { type: String },
   friend: [{ type: Schema.Types.ObjectId, ref: "User" }],
   createdComments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  participatedComments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  repliedComments: [
+    {
+      comment: { type: Schema.Types.ObjectId, ref: "Comment" },
+      text: { type: String },
+    },
+  ],
+  accessibleComments: [
+    {
+      comment: { type: Schema.Types.ObjectId, ref: "Comment" },
+      isChecked: { type: Boolean },
+    },
+  ],
 });
 
 exports.User = mongoose.model("User", userSchema);
