47dfe0b6d567b15c48fdc8f29c362198a51be6b9
sunghankwon <akvo852@naver.com>
2024-02-08 23:51:49 +0900

    feat: Add endpoint for client login request

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -2,12 +2,14 @@ const indexRouter = require("../routes/index");
 const loginRouter = require("../routes/login");
 const profileRouter = require("../routes/profile");
 const commentsRouter = require("../routes/comments");
+const locationRouter = require("../routes/location");
 
 async function routerLoader(app) {
   app.use("/", indexRouter);
   app.use("/login", loginRouter);
   app.use("/profile", profileRouter);
   app.use("/comments", commentsRouter);
+  app.use("/location", locationRouter);
 }
 
 module.exports = routerLoader;
--- src/routes/login.js
+++ src/routes/login.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { User } = require("../models/User");
+const verifyToken = require("../middleware/verifyToken");
 
 router.post("/", async (req, res, next) => {
   try {
@@ -16,6 +17,22 @@ router.post("/", async (req, res, next) => {
         nickname: userData.displayName ? userData.displayName : "Unnamed",
         icon: userData.photoURL,
       });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: "Login failed." });
+  }
+});
+
+router.post("/client", verifyToken, async (req, res, next) => {
+  try {
+    const userData = req.user;
+
+    let user = await User.findOne({ email: userData.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
     }
 
     res.status(200).json({ user });
