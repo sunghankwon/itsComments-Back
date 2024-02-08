505636a43e2bdc75d31184d9e3c6af284565c81b
sunghankwon <akvo852@naver.com>
2024-02-08 23:53:08 +0900

    fix: Update verifyToken middleware

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -8,26 +8,14 @@ admin.initializeApp({
 
 const verifyToken = async (req, res, next) => {
   try {
-    const accessToken = req.cookies.accessToken;
-    const refreshToken = req.cookies.refreshToken;
+    const userToken = req.body.token;
 
-    if (!accessToken || !refreshToken) {
+    if (!userToken) {
       return res.status(400).json({ message: "The token does not exist." });
     }
 
-    let decodedData;
-
-    try {
-      decodedData = await admin.auth().verifyIdToken(accessToken);
-    } catch (error) {
-      if (error.code === "auth/id-token-expired") {
-        const user = await admin.auth().verifyIdToken(refreshToken);
-        const newAccessToken = await user.getIdToken();
-        decodedData = await admin.auth().verifyIdToken(newAccessToken);
-      } else {
-        return res.status(400).json({ message: error.message });
-      }
-    }
+    const decodedData = await admin.auth().verifyIdToken(userToken);
+    req.user = decodedData;
 
     next();
   } catch (error) {
