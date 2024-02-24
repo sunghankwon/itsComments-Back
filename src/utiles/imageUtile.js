39c6731cbe8c90617ff8799f5db85fb56f3245be
sunghankwon <akvo852@naver.com>
2024-02-24 20:18:05 +0900

    refactor: Remove unused utility functions

--- src/utiles/imageUtile.js
+++
@@ -1,9 +0,0 @@
-const GOOGLE_PROFILE_IMAGE_PREFIX = process.env.GOOGLE_PROFILE_IMAGE_PREFIX;
-
-function isValidGoogleProfileImageUrl(url) {
-  const isValidUrl = url.startsWith(GOOGLE_PROFILE_IMAGE_PREFIX);
-
-  return isValidUrl;
-}
-
-module.exports = { isValidGoogleProfileImageUrl };
