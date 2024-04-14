18ef6d4b85e2eb6db4918e8e3f5884ac9a71502c
sunghankwon <akvo852@naver.com>
2024-04-14 19:34:50 +0900

    set: Configure Jest and setup testing environment

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGODB_TEST_URI
+    : process.env.MONGODB_URI;
 
 async function mongooseLoader() {
   try {
